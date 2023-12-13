ca006ea894806b0cdf97518a3477933a50e23298
Spark <91779642+SPARKY67966@users.noreply.github.com>
2023-12-13 18:46:18 +0530

    Update net.py

--- esp32/setup/net.py
+++ esp32/setup/net.py
@@ -1,27 +1,54 @@
 import network
 
-class net():
+class NetworkManager:
   
-  def __init__(self, ssid : str = None, password : str = None) -> None:
+    def __init__(self, ssid: str = None, password: str = None) -> None:
+        """
+        Initializes the NetworkManager with optional SSID and password for connection.
+
+        Args:
+            ssid (str): SSID of the Wi-Fi network.
+            password (str): Password of the Wi-Fi network.
+        """
+        self.ssid = ssid
+        self.station = network.WLAN(network.STA_IF)
+
+        # Check if not connected and no credentials provided
+        if not self.station.isconnected() and not (ssid and password):
+            raise ValueError('Connection unsuccessful\nCredentials are not provided')
+
+        # If not connected, activate station mode and connect
+        elif not self.station.isconnected():
+            self.station.active(True)
+            self.station.connect(ssid, password)
+            
+            # Wait until connection is established
+            while not self.station.isconnected():
+                pass
     
-    self.ssid = ssid
-    self.station = network.WLAN(network.STA_IF)
+    def get_device_ip(self) -> str:
+        """
+        Gets the local IP address of the device.
 
-    if not self.station.isconnected() and (ssid or password) is None:
-       raise ValueError('Connection unsuccesfull \n credentials are not provided')
-
-    elif not self.station.isconnected():
-        self.station.active(True)
-        self.station.connect(ssid,password)
-        while self.station.isconnected() == False:
-             pass
-    
-  def deviceip(self) -> str:
-    return self.station.ifconfig()[0] 
+        Returns:
+            str: Local IP address.
+        """
+        return self.station.ifconfig()[0] 
   
-  def connection_name(self) -> str:
-     return self.ssid
-  
-  def routerip(self) -> str:
-     return self.station.ifconfig()[-1]
+    def get_connection_name(self) -> str:
+        """
+        Gets the SSID of the connected network.
 
+        Returns:
+            str: SSID of the connected network.
+        """
+        return self.ssid
+  
+    def get_router_ip(self) -> str:
+        """
+        Gets the IP address of the router.
+
+        Returns:
+            str: Router IP address.
+        """
+        return self.station.ifconfig()[-1]
